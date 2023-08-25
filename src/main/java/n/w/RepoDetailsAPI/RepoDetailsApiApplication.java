4781c84ceb02e505636c863cfa45ba8f1749b0a4
NorbertWoj <norbert.wojcik.it@gmail.com>
2023-08-25 12:33:40 +0200

    Initial commit

---
+++ src/main/java/n/w/RepoDetailsAPI/RepoDetailsApiApplication.java
@@ -0,0 +1,13 @@
+package n.w.RepoDetailsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoDetailsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoDetailsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/n/w/RepoDetailsAPI/RepoDetailsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package n.w.RepoDetailsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoDetailsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
